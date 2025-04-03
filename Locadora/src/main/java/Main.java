f29c36a5ca54edac6a55c5b662b810b361e852a8
Gustavo Pisanello <gustavo.pisanello@etec.sp.gov.br>
2025-04-02 21:53:51 -0300

    Código finalizado

---
+++ Locadora/src/main/java/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.print(insereDados());
+    }
+
+    public static String insereDados(){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insira o modelo do veículo: ");
+        String modelo = sc.nextLine();
+
+        System.out.println("Insira a diária do veículo: ");
+        double valorDiaria = sc.nextDouble();
+        Veiculo vel = new Veiculo(modelo, valorDiaria);
+
+        System.out.println("Insira os dias de uso do veículo: ");
+        int diasDeUso = sc.nextInt();
+
+        System.out.println("Insira os dias de atraso do veículo: ");
+        int diasDeAtraso = sc.nextInt();
+
+        return (String.format("O valor à pagar é de: R$ %.2f", vel.calculaMultas(diasDeUso, diasDeAtraso)));
+
+    }
+}
---
+++ Locadora/src/test/java/TestVeiculo.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestVeiculo {
+    @Test
+    public void testCustoVeiculo(){
+        Veiculo vel = new Veiculo("Ford Ka", 80);
+        assertEquals(576, vel.custoVeiculo(8), 0);
+    }
+
+    @Test
+    public void testCustoVeiculo2(){
+        Veiculo vel = new Veiculo("Ford Ka", 80);
+        assertEquals(0, vel.custoVeiculo(0), 0);
+    }
+
+    @Test
+    public void testCustoVeiculo3(){
+        Veiculo vel = new Veiculo("Ford Ka", 80);
+        assertEquals(0, vel.custoVeiculo(-8), 0);
+    }
+
+    @Test
+    public void testCalculaMulta(){
+        Veiculo vel = new Veiculo("Ford Ka", 80);
+        assertEquals(3566.4401793024, vel.calculaMultas(8, 10), 0);
+    }
+
+
+}
